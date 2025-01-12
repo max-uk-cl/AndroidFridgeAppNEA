7b6f3711cabbfbfa15c75f314c8589ee32b15796
max-uk-cl <carlos.sepulveda.farfan@gmail.com>
2025-01-12 18:57:23 +0000

    "Take photo" Button now actually takes a photo and saves it with file name "productCode,jpg"

--- app/src/main/java/com/example/cameratestingapp/MainActivity.kt
+++ app/src/main/java/com/example/cameratestingapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.cameratestingapp
 
 import android.content.pm.PackageManager
+import android.media.Image
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
@@ -11,22 +13,29 @@ import com.example.cameratestingapp.databinding.ActivityMainBinding
 import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.cameratestingapp.Constants.TAG
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private var imageCapture: ImageCapture? = null
 
+    private lateinit var  outputDirectory: File
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        outputDirectory = getOutputDirectory()
         if (allPermissionGranted()) {
             startCamera()
         } else {
@@ -41,7 +50,57 @@ class MainActivity : AppCompatActivity() {
             insets
 
         }
+
+
+        binding.btnTakePhoto.setOnClickListener {
+            takePhoto()
+
+        }
     }
+
+    private fun getOutputDirectory(): File{
+        val mediaDir =  externalMediaDirs.firstOrNull()?.let { mFile ->
+            File(mFile, resources.getString(R.string.app_name)).apply {
+                mkdirs()
+            }
+        }
+
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+    private fun takePhoto(){
+        val imageCapture = imageCapture?:return
+        val photoFile = File(
+            outputDirectory,"productCode.jpg")
+
+
+        val outputOption = ImageCapture
+            .OutputFileOptions
+            .Builder(photoFile)
+            .build()
+
+
+        imageCapture.takePicture(
+            outputOption, ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback{
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo Saved"
+
+                    Toast.makeText(this@MainActivity,
+                        "$msg $savedUri",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(Constants.TAG,"onError: ${exception.message}", exception)
+                }
+            }
+        )
+    }
+
 
 
     private fun startCamera() {
